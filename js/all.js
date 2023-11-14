6e1c7fadf50cee4db1589f34443389bf86d87d9e
alycehwy <pray2451@gmail.com>
2023-11-15 03:07:03 +0800

    complete RWD mobile version footer

---
+++ js/all.js
@@ -0,0 +1,16 @@
+// const cover = document.querySelector('.cover');
+// const coverBlack = document.querySelector('.cover-black');
+// const coverBlackPos = coverBlack.getBoundingClientRect().right;
+
+
+// function windowResize(event){
+//     // console.log(coverBlack.offsetLeft);
+//     // console.dir(coverBlack.offsetLeft + coverBlack.clientWidth)
+//     // console.dir(coverBlack.getBoundingClientRect().right)
+//     console.log(event.target.innerWidth)
+//     if(event.target.innerWidth <= 1280){
+//         console.log('change');
+//     }
+// }
+
+// window.addEventListener("resize", windowResize);
